e5c7779d8ce3b151d6907a42f90b74a0c9183cc1
Richard <rsrprivat@gmail.com>
2021-06-17 13:19:39 +0200

    testing structure

---
+++ src/components/category/Category.js
@@ -0,0 +1,38 @@
+import "./category.scss";
+
+const Category = () => (
+  <>
+    <div className="category">
+      <div className="content">
+        <h1 className="title">ONE</h1>
+        <span className="shop-now">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="category">
+      <div className="content">
+        <h1 className="title">TWO</h1>
+        <span className="shop-now">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="category">
+      <div className="content">
+        <h1 className="title">THREE</h1>
+        <span className="shop-now">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="category">
+      <div className="content">
+        <h1 className="title">FOUR</h1>
+        <span className="shop-now">SHOP NOW</span>
+      </div>
+    </div>
+    <div className="category">
+      <div className="content">
+        <h1 className="title">FIVE</h1>
+        <span className="shop-now">SHOP NOW</span>
+      </div>
+    </div>
+  </>
+);
+
+export default Category;
---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import Category from "../../components/category/Category";
+
+import "./Homepage.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="categories">
+      <Category />
+    </div>
+  </div>
+);
+
+export default Homepage;
