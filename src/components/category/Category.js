2c5b0a796c383987c291107e19f36c7ef6af1bf8
Richard <rsrprivat@gmail.com>
2021-06-17 14:18:28 +0200

    some style

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,12 +1,13 @@
 import "./category.scss";
 
 const Category = ({ title, imageURL, size }) => (
-  <div
-    className={`category ${size}`}
-    style={{ backgroundImage: `url(${imageURL})` }}
-  >
+  <div className={`category ${size}`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageURL})` }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="shop-now">SHOP NOW</span>
     </div>
   </div>
