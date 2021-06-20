c6edec5cb6e37728d516e965142f696258aaccde
Richard <rsrprivat@gmail.com>
2021-06-20 17:09:33 +0200

    some more styling

--- src/components/category-item/CategoryItem.js
+++ src/components/category-item/CategoryItem.js
@@ -2,11 +2,10 @@ import "./categoryItem.scss";
 
 const CategoryItem = ({ id, name, price, imageURL }) => (
   <div className="category-item">
-    <div className="image" style={{ backgroundImage: `url(${imageURL})` }}>
-      <div className="product-info">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
+    <div className="image" style={{ backgroundImage: `url(${imageURL})` }} />
+    <div className="product-info">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
   </div>
 );
