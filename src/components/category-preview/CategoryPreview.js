b39222498b05c994c6ca885db1098ba58c69f577
Richard <rsrprivat@gmail.com>
2021-06-20 17:05:50 +0200

    some styling

--- src/components/category-preview/CategoryPreview.js
+++ src/components/category-preview/CategoryPreview.js
@@ -2,16 +2,14 @@ import CategoryItem from "../category-item/CategoryItem";
 import "./categoryPreview.scss";
 
 const CategoryPreview = ({ title, items }) => (
-  <div>
-    <h1>{title.toUpperCase()}</h1>
-    <div>
-      <div>
-        {items
-          .filter((item, index) => index < 4)
-          .map(({ id, ...itemProps }) => (
-            <CategoryItem key={id} {...itemProps} />
-          ))}
-      </div>
+  <div className="category-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...itemProps }) => (
+          <CategoryItem key={id} {...itemProps} />
+        ))}
     </div>
   </div>
 );
