b409c923cb59ac50bf8b0d817f2ae1217ace98f4
Richard <rsrprivat@gmail.com>
2021-06-20 17:38:07 +0200

    fixed some styling issues

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/Homepage";
 import Products from "./pages/products/Products";
+import Header from "./components/header/Header";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/products" component={Products} />
---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import "./header.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="nav-options">
+      <Link className="nav-option" to="/products">
+        Products
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
