3a329c2199f39103153494e3478db4b623f40725
Richard <rsrprivat@gmail.com>
2021-06-21 09:53:40 +0200

    testing sign in and out

--- src/App.js
+++ src/App.js
@@ -31,18 +31,24 @@ class App extends React.Component {
     };
   }
 
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
       this.setState({ currentUser: user });
 
       console.log(user);
     });
   }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/products" component={Products} />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+
 import "./header.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="nav-options">
-      <Link className="nav-option" to="/signin">
-        Sign in
-      </Link>
       <Link className="nav-option" to="/products">
         Products
       </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="nav-option" to="/signin">
+          Sign In
+        </Link>
+      )}
     </div>
   </div>
 );
