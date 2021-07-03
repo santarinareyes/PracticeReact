fbde69ad6a142ab451750d6fac8db24e2fae0c67
Richard Reyes <rsrprivat@gmail.com>
2021-07-03 22:06:32 +0200

    getting quant for cart icon

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -5,10 +5,10 @@ import { ReactComponent as ShoppingBagIcon } from "../../assets/shopping-bag.svg
 
 import "./cartIcon.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingBagIcon className="shopping-bag-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -16,4 +16,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((prev, cartItem) => prev + cartItem.quantity, 0),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
