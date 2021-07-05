771329ae97fa51e46b2b4ca74cfae88127d2e56d
Richard Reyes <rsrprivat@gmail.com>
2021-07-05 05:41:44 +0200

    cart selector test

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,5 +1,7 @@
 import { connect } from "react-redux";
+
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import { ReactComponent as ShoppingBagIcon } from "../../assets/shopping-bag.svg";
 
@@ -16,8 +18,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce((prev, cartItem) => prev + cartItem.quantity, 0),
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
