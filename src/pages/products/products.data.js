0bef8d3456ff0291d3f48b68f5e86616ccc376af
Richard Reyes <rsrprivat@gmail.com>
2021-07-12 14:45:45 +0200

    some changes to data

--- src/pages/products/products.data.js
+++ src/pages/products/products.data.js
@@ -1,212 +1,212 @@
-export const collections = [
-  {
+export const collections = {
+  one: {
     id: 1,
-    title: "One",
-    route: "one",
+    title: 'One',
+    route: 'one',
     items: [
       {
         id: 1,
-        name: "One One",
+        name: 'One One',
         imageURL:
-          "https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
+          'https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
         price: 1,
       },
       {
         id: 2,
-        name: "One Two",
+        name: 'One Two',
         imageURL:
-          "https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
+          'https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
         price: 11,
       },
       {
         id: 3,
-        name: "One Three",
+        name: 'One Three',
         imageURL:
-          "https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
+          'https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
         price: 111,
       },
       {
         id: 4,
-        name: "One Four",
+        name: 'One Four',
         imageURL:
-          "https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
+          'https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
         price: 1111,
       },
       {
         id: 5,
-        name: "One Five",
+        name: 'One Five',
         imageURL:
-          "https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80",
+          'https://images.unsplash.com/photo-1613057389222-5c429f4ec162?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80',
         price: 11111,
       },
     ],
   },
-  {
+  two: {
     id: 2,
-    title: "Two",
-    route: "two",
+    title: 'Two',
+    route: 'two',
     items: [
       {
         id: 6,
-        name: "Two One",
+        name: 'Two One',
         imageURL:
-          "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         price: 2,
       },
       {
         id: 7,
-        name: "Two Two",
+        name: 'Two Two',
         imageURL:
-          "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         price: 22,
       },
       {
         id: 8,
-        name: "Two Three",
+        name: 'Two Three',
         imageURL:
-          "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         price: 222,
       },
       {
         id: 9,
-        name: "Two Four",
+        name: 'Two Four',
         imageURL:
-          "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         price: 2222,
       },
       {
         id: 10,
-        name: "Two Five",
+        name: 'Two Five',
         imageURL:
-          "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         price: 22222,
       },
     ],
   },
-  {
+  three: {
     id: 3,
-    title: "Three",
-    route: "three",
+    title: 'Three',
+    route: 'three',
     items: [
       {
         id: 11,
-        name: "Three One",
+        name: 'Three One',
         imageURL:
-          "https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
         price: 3,
       },
       {
         id: 12,
-        name: "Three Two",
+        name: 'Three Two',
         imageURL:
-          "https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
         price: 33,
       },
       {
         id: 13,
-        name: "Three Three",
+        name: 'Three Three',
         imageURL:
-          "https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
         price: 333,
       },
       {
         id: 14,
-        name: "Three Four",
+        name: 'Three Four',
         imageURL:
-          "https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
         price: 3333,
       },
       {
         id: 15,
-        name: "Three Five",
+        name: 'Three Five',
         imageURL:
-          "https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80",
+          'https://images.unsplash.com/photo-1535622380377-1e347c44a12d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
         price: 33333,
       },
     ],
   },
-  {
+  four: {
     id: 4,
-    title: "Four",
-    route: "four",
+    title: 'Four',
+    route: 'four',
     items: [
       {
         id: 16,
-        name: "Four One",
+        name: 'Four One',
         imageURL:
-          "https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          'https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
         price: 4,
       },
       {
         id: 17,
-        name: "Four Two",
+        name: 'Four Two',
         imageURL:
-          "https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          'https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
         price: 44,
       },
       {
         id: 18,
-        name: "Four Three",
+        name: 'Four Three',
         imageURL:
-          "https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          'https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
         price: 444,
       },
       {
         id: 19,
-        name: "Four Four",
+        name: 'Four Four',
         imageURL:
-          "https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          'https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
         price: 4444,
       },
       {
         id: 20,
-        name: "Four Five",
+        name: 'Four Five',
         imageURL:
-          "https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+          'https://images.unsplash.com/photo-1597662942557-4087865bf476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80',
         price: 44444,
       },
     ],
   },
-  {
+  five: {
     id: 5,
-    title: "Five",
-    route: "five",
+    title: 'Five',
+    route: 'five',
     items: [
       {
         id: 21,
-        name: "Five One",
+        name: 'Five One',
         imageURL:
-          "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80",
+          'https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80',
         price: 5,
       },
       {
         id: 22,
-        name: "Five Two",
+        name: 'Five Two',
         imageURL:
-          "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80",
+          'https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80',
         price: 55,
       },
       {
         id: 23,
-        name: "Five Three",
+        name: 'Five Three',
         imageURL:
-          "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80",
+          'https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80',
         price: 555,
       },
       {
         id: 24,
-        name: "Five Four",
+        name: 'Five Four',
         imageURL:
-          "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80",
+          'https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80',
         price: 5555,
       },
       {
         id: 25,
-        name: "Five Five",
+        name: 'Five Five',
         imageURL:
-          "https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80",
+          'https://images.unsplash.com/photo-1519150268069-c094cfc0b3c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1078&q=80',
         price: 55555,
       },
     ],
   },
-];
+}
--- src/redux/product/product.selectors.js
+++ src/redux/product/product.selectors.js
@@ -1,14 +1,6 @@
 import memoize from 'lodash.memoize'
 import { createSelector } from 'reselect'
 
-const COLLECTION_ID_MAP = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-}
-
 const selectProduct = state => state.product
 
 export const selectCollections = createSelector(
@@ -17,9 +9,8 @@ export const selectCollections = createSelector(
 )
 
 export const selectCollection = memoize(collectionUrlParam =>
-  createSelector([selectCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
   )
 )
