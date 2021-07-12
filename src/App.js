9e7c3514660c36d43c6062c854d68ba7041ff138
Richard Reyes <rsrprivat@gmail.com>
2021-07-12 16:14:37 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import Homepage from './pages/homepage/Homepage'
-import Products from './pages/products/Product'
+import Products from './pages/products/Products'
 import SignInOrSignUp from './pages/signInOrSignUp/SignInOrSignUp'
 import Checkout from './pages/checkout/Checkout'
 
