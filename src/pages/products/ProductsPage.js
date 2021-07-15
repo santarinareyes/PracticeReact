f758186d6a253d78a90564fe708d6e5f9bd0234f
Richard Reyes <rsrprivat@gmail.com>
2021-07-15 15:08:56 +0200

    fetching from firebase test

--- src/pages/products/ProductsPage.js
+++ src/pages/products/ProductsPage.js
@@ -1,12 +1,29 @@
+import { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
 import Collection from '../collection/Collection'
 
-const ProductsPage = ({ match }) => (
-  <div>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collection`} component={Collection} />
-  </div>
-)
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils'
+
+const ProductsPage = ({ match }) => {
+  const unsubscribeFromSnapshot = null
+
+  useEffect(() => {
+    const collectionRef = firestore.collection('collections')
+    collectionRef.onSnapshot(async snapshot => {
+      convertCollectionsSnapshotToMap(snapshot)
+    })
+  }, [])
+
+  return (
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collection`} component={Collection} />
+    </div>
+  )
+}
 
 export default ProductsPage
