cb9bf4ed8a57af45145c4727666c557742c91a10
Richard Reyes <rsrprivat@gmail.com>
2021-07-16 12:03:18 +0200

    coverted App to functional component

--- src/App.js
+++ src/App.js
@@ -15,13 +15,11 @@ import { selectCurrentUser } from './redux/user/user.selectors'
 import { setCurrentUser } from './redux/user/user.actions'
 
 import './App.css'
+import { useEffect } from 'react'
 
-class App extends React.Component {
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+const App = ({ currentUser, setCurrentUser }) => {
+  useEffect(() => {
+    auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDoc(userAuth)
         userRef.onSnapshot(snapShot => {
@@ -33,32 +31,72 @@ class App extends React.Component {
       }
       setCurrentUser(userAuth)
     })
-  }
+  }, [setCurrentUser])
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth()
-  }
+  console.log('app rendered')
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/checkout' component={Checkout} />
-          <Route
-            exact
-            path='/signin'
-            render={() =>
-              this.props.currentUser ? <Redirect to='/' /> : <SignInOrSignUp />
-            }
-          />
-          <Route path='/products' component={ProductsPage} />
-        </Switch>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/checkout' component={Checkout} />
+        <Route
+          exact
+          path='/signin'
+          render={() =>
+            currentUser ? <Redirect to='/' /> : <SignInOrSignUp />
+          }
+        />
+        <Route path='/products' component={ProductsPage} />
+      </Switch>
+    </div>
+  )
 }
+
+// class App extends React.Component {
+//   unsubscribeFromAuth = null
+
+//   componentDidMount() {
+//     const { setCurrentUser } = this.props
+//     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+//       if (userAuth) {
+//         const userRef = await createUserProfileDoc(userAuth)
+//         userRef.onSnapshot(snapShot => {
+//           setCurrentUser({
+//             id: snapShot.id,
+//             ...snapShot.data(),
+//           })
+//         })
+//       }
+//       setCurrentUser(userAuth)
+//     })
+//   }
+
+//   componentWillUnmount() {
+//     this.unsubscribeFromAuth()
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Switch>
+//           <Route exact path='/' component={Homepage} />
+//           <Route exact path='/checkout' component={Checkout} />
+//           <Route
+//             exact
+//             path='/signin'
+//             render={() =>
+//               this.props.currentUser ? <Redirect to='/' /> : <SignInOrSignUp />
+//             }
+//           />
+//           <Route path='/products' component={ProductsPage} />
+//         </Switch>
+//       </div>
+//     )
+//   }
+// }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
