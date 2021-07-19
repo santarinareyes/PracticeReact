31a53553506b97a8f0b43665d5a3401812d2ff26
Richard Reyes <rsrprivat@gmail.com>
2021-07-19 11:44:55 +0200

    fetching collections using saga

--- src/redux/product/product.sagas.js
+++ src/redux/product/product.sagas.js
@@ -1,8 +1,23 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, call, put } from 'redux-saga/effects'
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from '../../firebase/firebase.utils'
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from './product.actions'
 import ProductActionTypes from './product.types'
 
 export function* fetchCollectionsAsync() {
-  yield console.log('Yielded')
+  try {
+    const collectionRef = firestore.collection('collections')
+    const snapshot = yield collectionRef.get()
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+    yield put(fetchCollectionsSuccess(collectionsMap))
+  } catch (err) {
+    yield put(fetchCollectionsFailure(err.message))
+  }
 }
 
 export function* fetchCollectionsStart() {
