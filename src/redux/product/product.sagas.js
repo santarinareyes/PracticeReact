a0cd121763b3c75f76efdb1b18b8bcb4fee811f4
Richard Reyes <rsrprivat@gmail.com>
2021-07-20 16:12:39 +0200

    added same pattern to product saga

--- src/redux/product/product.sagas.js
+++ src/redux/product/product.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 import {
   convertCollectionsSnapshotToMap,
   firestore,
@@ -25,4 +25,8 @@ export function* fetchCollectionsStart() {
     ProductActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   )
+}
+
+export function* productSagas() {
+  yield all([call(fetchCollectionsSuccess)])
 }
