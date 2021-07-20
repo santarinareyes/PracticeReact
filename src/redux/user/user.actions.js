4b8bdad31d05d75e9f32f592409efa7bfe384218
Richard Reyes <rsrprivat@gmail.com>
2021-07-20 10:52:12 +0200

    added user actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,34 @@
-import { UserActionTypes } from "./user.types";
+import { UserActionTypes } from './user.types'
 
-export const setCurrentUser = (user) => ({
+export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
-});
+})
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+})
+
+export const googleSignInSuccess = user => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+})
+
+export const googleSignInFailure = user => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+})
+
+export const emailSignInStart = () => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+})
+
+export const emailSignInSuccess = user => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+})
+
+export const emailSignInFailure = user => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+})
