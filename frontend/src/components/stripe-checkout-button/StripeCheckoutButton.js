5669e4c2c8e419f55fb221ca9da0636cef3d837a
Richard Reyes <rsrprivat@gmail.com>
2021-07-21 14:58:19 +0200

    payment working

--- frontend/src/components/stripe-checkout-button/StripeCheckoutButton.js
+++ frontend/src/components/stripe-checkout-button/StripeCheckoutButton.js
@@ -1,4 +1,5 @@
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
@@ -6,8 +7,21 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51Io0YXD6lU6s8HD0tIwNHDBt1BZ7x3eMIhl3uWtLkktUgmXaUQjCHmMLYNkQAjPJOcJ19luoA6DGH7Lceb9p2E4v00TUSj0GAR'
 
   const onToken = token => {
-    console.log('token', token)
-    alert('Payment successful')
+    const data = {
+      amount: priceForStripe,
+      token,
+    }
+
+    axios
+      .post('payment', data)
+      .then(response => {
+        console.log('response', response)
+        alert('Payment successful')
+      })
+      .catch(err => {
+        console.log('err', JSON.parse(err))
+        alert('Make sure to use 4242 4242 4242 4242, 01/22, 123')
+      })
   }
 
   return (
@@ -16,7 +30,7 @@ const StripeCheckoutButton = ({ price }) => {
       name='My E-commerce'
       billingAddress
       shippingAddress
-      description={`Your total is $${price}`}
+      description={`Your total is ${price} SEK`}
       amount={priceForStripe}
       panelLabel='Pay Now2'
       token={onToken}
