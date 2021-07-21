e85ad1360a5b79085f306cae8051d4f8902f0d09
Richard Reyes <rsrprivat@gmail.com>
2021-07-21 16:20:34 +0200

    removed src and public folders

--- src/components/custom-button/CustomButton.js
+++
@@ -1,7 +0,0 @@
-import * as S from './styles'
-
-const CustomButton = ({ children, ...props }) => (
-  <S.CustomButtonContainer {...props}>{children}</S.CustomButtonContainer>
-)
-
-export default CustomButton
