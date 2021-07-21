2d60015d114732c6495da1c86b8df1d40d8422c1
Richard Reyes <rsrprivat@gmail.com>
2021-07-21 14:43:07 +0200

    backend payment

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')))
@@ -24,4 +23,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error
   console.log('Server running on port ' + port)
+})
+
+app.post('/payment', (request, response) => {
+  const body = {
+    source: request.body.token.id,
+    amount: request.body.amount,
+    currency: 'sek',
+  }
+
+  stripe.charges.create(body, (stripeErr, stripeResponse) => {
+    if (stripeErr) {
+      response.status(500).send({ error: stripeErr })
+    } else {
+      response.status(200).send({ success: stripeResponse })
+    }
+  })
 })
