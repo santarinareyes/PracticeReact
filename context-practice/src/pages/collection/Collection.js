5725ef54fbcdca7c883dcf16cb4984cc3f5e6f67
Richard Reyes <rsrprivat@gmail.com>
2021-07-21 17:22:40 +0200

    with useContext

--- context-practice/src/pages/collection/Collection.js
+++ context-practice/src/pages/collection/Collection.js
@@ -3,26 +3,21 @@ import CategoryItem from '../../components/category-item/CategoryItem'
 import { selectCollection } from '../../redux/product/product.selectors'
 import './collection.scss'
 import CollectionsContext from '../../contexts/collections/collections.context'
+import { useContext } from 'react'
 
-const Collection = ({ collection, match }) => {
+const Collection = ({ match }) => {
+  const collections = useContext(CollectionsContext)
+  const collection = collections[match.params.collection]
+  const { title, items } = collection
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collection]
-        const { title, items } = collection
-
-        return (
-          <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-              {items.map(item => (
-                <CategoryItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        )
-      }}
-    </CollectionsContext.Consumer>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CategoryItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   )
 }
 
