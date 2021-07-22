bcffef2840642309d7cb1b10e01b8fdd789dafc8
Richard Reyes <rsrprivat@gmail.com>
2021-07-22 14:06:44 +0200

    filter item util added

--- context-practice/src/redux/cart/cart.utils.js
+++ context-practice/src/redux/cart/cart.utils.js
@@ -1,31 +1,34 @@
 export const cartItemQuantity = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToAdd.id
-  );
+    cartItem => cartItem.id === cartItemToAdd.id
+  )
 
   if (existingCartItem) {
-    return cartItems.map((cartItem) =>
+    return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
-    );
+    )
   }
 
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-};
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToRemove.id
-  );
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
 
   if (existingCartItem.quantity === 1) {
-    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
   }
 
-  return cartItems.map((cartItem) =>
+  return cartItems.map(cartItem =>
     cartItem.id === cartItemToRemove.id
       ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
-  );
-};
+  )
+}
+
+export const filterItemFromCart = (cartItems, cartItemToClear) =>
+  cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id)
