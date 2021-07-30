169b0860b7fda01edecf4832caac1160fc8b1546
Richard Reyes <rsrprivat@gmail.com>
2021-07-30 12:24:23 +0200

    so that we are not required to use https in development

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
   app.use(express.static(path.join(__dirname, 'frontend/build')))
 
   app.get('*', function (request, response) {
