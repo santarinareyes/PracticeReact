0e1fa459d12d26a2b1bdc1327611b1c8de10c391
Richard Reyes <rsrprivat@gmail.com>
2021-08-09 12:39:46 +0200

    removed from repo

--- context-practice/src/contexts/collections/collections.context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react'
-import { collections } from '../../pages/products/products.data'
-
-const CollectionsContext = createContext(collections)
-
-export default CollectionsContext
